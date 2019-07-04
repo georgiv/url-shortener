e64c26cdb12c4c5a41e633fb1f250e8b5d48829e
i307073 <georgi.vasilev@sap.com>
2019-07-04 18:04:43 +0300

    Working on it...

--- server/db/db_worker.go
+++ server/db/db_worker.go
@@ -1,9 +1,35 @@
 package db
 
+import (
+	"database/sql"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type DbConfiguration struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+}
+
 type DbWorker interface {
 	Persist() error
 }
 
 type db struct {
+	con *sql.DB
+}
 
+func NewDbWorker() (DbWorker, error) {
+	con, err := sql.Open("mysql", "cranki:abcd1234@tcp(127.0.0.1:3306)/url_shortener")
+	if err != nil {
+		return nil, err
+	}
+
+	return &db{con: con}, nil
+}
+
+func (*db) Persist() error {
+	return nil
 }
--- server/web/server.go
+++ server/web/server.go
@@ -11,44 +11,49 @@ import (
 )
 
 type Server interface {
-	Handle()	
+	Handle()
 }
 
 type urlServer struct {
-	host string
-	port int
-	db db.DbWorker
+	host     string
+	port     int
+	dbWorker db.DbWorker
 }
 
 type urlAlias struct {
-	original string
-	custom string
+	Original string `json:"original"`
+	Custom   string `json:"custom"`
 }
 
-func NewServer(host string, port int) Server {
-	return &urlServer{host: host, port: port}
+func NewServer(host string, port int) (Server, error) {
+	dbWorker, err := db.NewDbWorker()
+	if err != nil {
+		return nil, err
+	}
+
+	return &urlServer{host: host, port: port, dbWorker: dbWorker}, nil
 }
 
 func (s *urlServer) Handle() {
 	r := mux.NewRouter()
-	r.HandleFunc("/api/urls/{id}", s.getUrl).Methods("GET")
-	r.HandleFunc("/api/urls", s.addUrl).Methods("POST")
+	r.HandleFunc("/api/urls/{id}", s.getURL).Methods("GET")
+	r.HandleFunc("/api/urls", s.addURL).Methods("POST")
 	http.ListenAndServe(fmt.Sprintf("%v:%v", s.host, s.port), r)
 }
 
-func (s *urlServer) getUrl(w http.ResponseWriter, r *http.Request) {
+func (s *urlServer) getURL(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Get URL %v", mux.Vars(r)["id"])
 }
 
-func (s *urlServer) addUrl(w http.ResponseWriter, r *http.Request) {
+func (s *urlServer) addURL(w http.ResponseWriter, r *http.Request) {
 	body, err := ioutil.ReadAll(r.Body)
-    if err != nil {
-        panic(err)
+	if err != nil {
+		panic(err)
 	}
 
 	var b urlAlias
 	json.Unmarshal(body, &b)
 	fmt.Println(b)
-	w.Header().Set("Location", "http://localost:8888/dummy")
+	//w.Header().Set("Location", "http://localost:8888/dummy")
 	fmt.Fprintf(w, "Add URL")
 }
